614aff0b9314b523c9a15ca838396bf31826aaba
hardo <i@hardo.me>
2020-05-19 18:29:01 +0800

    #3183 Update deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ pwd
 
 VERSION=${1:-$(date +%s)};
 
-echo "deploying '$VERSION' makeflow issue synchronizer..."
+echo "deploying '$VERSION' of makeflow issue synchronizer..."
 
 VERSION=$VERSION docker-compose build
 VERSION=$VERSION docker stack deploy --compose-file docker-compose.yml issue-synchronizer
